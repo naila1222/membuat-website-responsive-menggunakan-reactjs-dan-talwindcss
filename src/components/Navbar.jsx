dd3a0a736b2310c7566f7948369c612870d77990
naila1222 <nailamalihatlnailamalihatu@gmail.com>
2025-09-18 15:20:38 +0700

    membuat website responsive menggunakan reactjs dan talwindcss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return <div className="Navbar">
+  <div className="Container">
+ <div className="Navbar-box">
+    <div className="Logo"></div>
+        <h1 className="text-2xl font-bold">ngoding</h1>
+    <ul>
+        <li>
+            <a href="#"></a>
+        </li>
+    </ul>
+     <div className="sosial"></div>
+ </div>
+  </div>
+  </div>  
+}
+
+export default Navbar
